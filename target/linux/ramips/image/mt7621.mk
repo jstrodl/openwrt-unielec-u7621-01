7d1ea01e8eb90417e660dabf5fb7bfe6e4aaf717
Johannes Strodl <johannes.strodl@kaffeesud.org>
2020-10-05 20:38:36 +0200

    Add basic support for U7621-01.

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -296,6 +296,14 @@ define Device/timecloud
 endef
 TARGET_DEVICES += timecloud
 
+define Device/u7621-01-256M-16M
+  DTS := U7621-01-256M-16M
+  IMAGE_SIZE := 16064k
+  DEVICE_TITLE := UniElec U7621-01 (256M RAM/16M flash)
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad-mini
+endef
+TARGET_DEVICES += u7621-01-256M-16M
+
 define Device/u7621-06-256M-16M
   DTS := U7621-06-256M-16M
   IMAGE_SIZE := 16064k
